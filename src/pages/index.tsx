c84b95152bf36d345c4c976614742bf1a2f738a0
Diogo Bruni <diogobruni@gmail.com>
2022-07-06 04:55:57 -0300

    Init project next.js with tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className='text-4xl font-bold'>
+        Hello World
+      </h1>
+    </>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+
+      }
+    },
+  },
+  plugins: [],
+}
