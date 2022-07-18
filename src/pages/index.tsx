ab90f99368e3b6fc8ac1a8f9fbae69d9caf69819
Diogo Bruni <diogobruni@gmail.com>
2022-07-18 06:22:13 -0300

    Improve home listing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,19 @@ import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil';
 
 import useSpotify from '../hooks/useSpotify';
-import { homeCategoriesPlaylists } from '../atoms/playlistAtom';
 
 import Loading from '../components/Loading';
 import PlaylistCard from '../components/PlaylistCard';
 import { CategoryWithPlaylistProps } from '../types/category.type';
 
-// import { spotifyApi } from '../lib/spotify'
-
-// interface CategoryWithPlaylistsProps extends SpotifyApi.CategoryObject {
-//   playlists?: SpotifyApi.PlaylistObjectSimplified[]
-// }
-
 const Home: NextPage = () => {
   const spotifyApi = useSpotify()
 
-  // let isFetching = false
   const [isFetching, setIsFetching] = useState(false)
-  // const [sectionPlaylists, setSectionPlaylists] = useState<CategoryWithPlaylistsProps[]>([])
-  const [sectionPlaylists, setSectionPlaylists] = useRecoilState(homeCategoriesPlaylists)
+  const [sectionPlaylists, setSectionPlaylists] = useState<CategoryWithPlaylistProps[]>([])
 
   useEffect(() => {
-    if (!spotifyApi || !spotifyApi.getAccessToken()) return
+    if (!spotifyApi) return
     if (sectionPlaylists.length) return
     if (isFetching) return
     setIsFetching(true)
@@ -63,7 +54,7 @@ const Home: NextPage = () => {
     }
 
     getCategories()
-  }, [spotifyApi])
+  }, [])
 
   if (!sectionPlaylists.length)
     return <Loading />
@@ -71,17 +62,22 @@ const Home: NextPage = () => {
   return (
     <main className="p-8 space-y-12">
 
-      {sectionPlaylists.map(({ id, name, playlists }) => (
-        <div key={id}>
-          <h2 className="text-2xl font-semibold mb-5">{name}</h2>
+      {sectionPlaylists.map(({ id, name, playlists }) => {
+        if (!playlists || !playlists.length) return true
+        return (
+          <div key={id}>
+            <h2 className="text-2xl font-semibold mb-5">{name}</h2>
 
-          <div className="grid grid-cols-3 gap-4 xl:grid-cols-6 xl:gap-6">
-            {playlists?.map(playlist => (
-              <PlaylistCard key={playlist.id} playlist={playlist} />
-            ))}
+            <div className="grid grid-cols-3 gap-4 xl:grid-cols-6 xl:gap-6">
+              {playlists?.map(playlist => {
+                return (
+                  <PlaylistCard key={playlist.id} playlist={playlist} />
+                )
+              })}
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
 
     </main>
   )
